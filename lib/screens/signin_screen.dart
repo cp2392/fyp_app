b6bc516df708aa88573136f3f938b2ecedc4e908
cp2392 <chiragpatil2392@gmail.com>
2024-04-01 23:03:17 +0530

    updated the signin page, but the image is not yet showing

---
+++ lib/screens/signin_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:fyp_app/utils/colors_utils.dart';
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({super.key});
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+          hexStringtToColor("CB2B93"),
+          hexStringtToColor("9546C4"),
+          hexStringtToColor("5E61F4")
+        ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+        child: SingleChildScrollView(
+            child: Padding(
+          padding: EdgeInsets.fromLTRB(
+              20, MediaQuery.of(context).size.height * 0.2, 20, 0),
+          child: Column(
+            children: <Widget>[
+              logoWidget("assets/images/logo1.png"),
+            ],
+          ),
+        )),
+      ),
+    );
+  }
+
+  Image logoWidget(String imageName) {
+    return Image.asset(
+      imageName,
+      fit: BoxFit.fitWidth,
+      // width: 240,
+      // height: 240,
+    );
+  }
+
+
+}
