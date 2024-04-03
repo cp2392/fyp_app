f00f275b09de926157ab6d0b92358235ec78fcb0
cp2392 <chiragpatil2392@gmail.com>
2024-04-03 22:47:01 +0530

    signup and signin screen ui done; both routing to home

--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fyp_app/reusable_widgets/resuable_widget.dart';
+import 'package:fyp_app/screens/home_screen.dart';
 import 'package:fyp_app/screens/signup_screen.dart';
 import 'package:fyp_app/utils/colors_utils.dart';
 
@@ -11,7 +12,6 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
-
   TextEditingController _passwordTextController = TextEditingController();
   TextEditingController _emailTextController = TextEditingController();
   @override
@@ -25,8 +25,7 @@ class _SignInScreenState extends State<SignInScreen> {
           hexStringtToColor("FFFFFF"),
           hexStringtToColor("FFFFFF"),
           hexStringtToColor("000000")
-        ], 
-        begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+        ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
         child: SingleChildScrollView(
             child: Padding(
           padding: EdgeInsets.fromLTRB(
@@ -37,15 +36,18 @@ class _SignInScreenState extends State<SignInScreen> {
               SizedBox(
                 height: 50,
               ),
-              reusableTextField("Enter Username", Icons.person_outline, false, _emailTextController),
+              reusableTextField("Enter Username", Icons.person_outline, false,
+                  _emailTextController),
               SizedBox(
                 height: 30,
               ),
-              reusableTextField("Enter Password", Icons.lock_outline, true, _passwordTextController),
-              SizedBox(
-                height: 30
-              ),
-              signInSignUpButton(context, true, (){}),
+              reusableTextField("Enter Password", Icons.lock_outline, true,
+                  _passwordTextController),
+              SizedBox(height: 30),
+              signInSignUpButton(context, true, () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => HomeScreen()));
+              }),
               signUpOption()
             ],
           ),
@@ -54,16 +56,16 @@ class _SignInScreenState extends State<SignInScreen> {
     );
   }
 
-
   Row signUpOption() {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        const Text("Don't have account? ", style: TextStyle(color: Colors.white70)),
+        const Text("Don't have account? ",
+            style: TextStyle(color: Colors.white70)),
         GestureDetector(
           onTap: () {
             Navigator.push(context,
-            MaterialPageRoute(builder: (context) => SignUpScreen()));
+                MaterialPageRoute(builder: (context) => SignUpScreen()));
           },
           child: const Text(
             "Sign Up",
@@ -73,5 +75,4 @@ class _SignInScreenState extends State<SignInScreen> {
       ],
     );
   }
-
 }
