2fcf8dc417394b7b0952f8cd510afb2131a57cf3
Chirag <chiragpatil2392@gmail.com>
2024-04-04 13:32:06 +0530

    sign up and sign in screen routing to home screen

--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:fyp_app/reusable_widgets/reusable_widget.dart';
+import 'package:fyp_app/screens/home_screen.dart';
+import 'package:fyp_app/screens/signup_screen.dart';
 import 'package:fyp_app/utils/colors_utils.dart';
 
 class SignInScreen extends StatefulWidget {
@@ -9,6 +12,8 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  TextEditingController _passwordTextController = TextEditingController();
+  TextEditingController _emailTextController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,11 +22,10 @@ class _SignInScreenState extends State<SignInScreen> {
         height: MediaQuery.of(context).size.height,
         decoration: BoxDecoration(
             gradient: LinearGradient(colors: [
-          hexStringtToColor("CB2B93"),
-          hexStringtToColor("9546C4"),
-          hexStringtToColor("5E61F4")
-        ], 
-        begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+          hexStringtToColor("FFFFFF"),
+          hexStringtToColor("FFFFFF"),
+          hexStringtToColor("000000")
+        ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
         child: SingleChildScrollView(
             child: Padding(
           padding: EdgeInsets.fromLTRB(
@@ -29,6 +33,22 @@ class _SignInScreenState extends State<SignInScreen> {
           child: Column(
             children: <Widget>[
               logoWidget("assets/images/logo1.png"),
+              SizedBox(
+                height: 50,
+              ),
+              reusableTextField("Enter Username", Icons.person_outline, false,
+                  _emailTextController),
+              SizedBox(
+                height: 30,
+              ),
+              reusableTextField("Enter Password", Icons.lock_outline, true,
+                  _passwordTextController),
+              SizedBox(height: 30),
+              signInSignUpButton(context, true, () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => HomeScreen()));
+              }),
+              signUpOption()
             ],
           ),
         )),
@@ -36,15 +56,23 @@ class _SignInScreenState extends State<SignInScreen> {
     );
   }
 
-  Image logoWidget(String imageName) {
-    return Image.asset(
-      imageName,
-      fit: BoxFit.fitWidth,
-      width: 240,
-      height: 240,
-      color: Colors.white,
+  Row signUpOption() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("Don't have account? ",
+            style: TextStyle(color: Colors.white70)),
+        GestureDetector(
+          onTap: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => SignUpScreen()));
+          },
+          child: const Text(
+            "Sign Up",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        )
+      ],
     );
   }
-
-
 }
